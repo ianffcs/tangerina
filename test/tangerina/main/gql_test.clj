800a3c7f4ade78a95f4b74736c3b68e37cd6943e
Enzzo Cavallo <souenzzo@gmail.com>
2020-05-23 19:33:41 -0300

    + lacinia-pedestal

--- test/tangerina/main/gql_test.clj
+++ test/tangerina/main/gql_test.clj
@@ -4,16 +4,19 @@
             [io.pedestal.test :refer [response-for]]
             [datascript.core :as ds]
             [com.walmartlabs.lacinia :as lacinia]
+            [com.walmartlabs.lacinia.pedestal2 :as lp]
             [clojure.data.json :as json]
             [com.walmartlabs.lacinia.schema :as lacinia.schema]
             [com.walmartlabs.lacinia.util :as lacinia.util]
             [com.wsscode.pathom.connect.graphql2 :as pcgql]
             [datascript.core :as d]))
 
+;; app.core
+
 (defn atom-impl
   [{::keys [state]}]
   (letfn [(next-id []
-            (swap! state update ::last-id (fnil inc 0)))]
+            (::last-id (swap! state update ::last-id (fnil inc 0))))]
     {:query/tasks          (fn [_ _ _]
                              (vals (get @state :task/by-id)))
      :mutation/create-task (fn [_ {:keys [description]} _]
@@ -63,52 +66,48 @@
                              (atom-impl {::state state})
                              #_(datascrip-impl {::conn conn}))
                            (lacinia.schema/compile))
-        http-service (-> {::http/routes #{["/graphql" :post
-                                           [{:name  ::error
-                                             :error (fn [ctx ex]
-                                                      (.printStackTrace ex)
-                                                      (assoc ctx :response {:body   (json/write-str {:ex-message (ex-message ex)})
-                                                                            :status 400}))}
-                                            (fn [{:keys [body] :as req}]
-                                              (let [{:keys [query variables]} (json/read-str (slurp body)
-                                                                                             :key-fn keyword)
-                                                    result (lacinia/execute lacinia-schema query variables req)]
-                                                {:body   (json/write-str result)
-                                                 :status 200}))]
-                                           :route-name ::graphql]}}
-                         http/default-interceptors)]
+        http-service (lp/default-service lacinia-schema {})]
     (assoc env ::conn conn
                ::state state
                ::http-service http-service)))
 
-(defn ->test-system
-  [{::keys [http-service]
-    :as    env}]
-  (assoc env ::service-fn (-> http-service
-                              http/create-servlet
-                              ::http/service-fn)))
+;; app.main
 
 (defn start-system
   "start with (-> (create-system {}) start-system)"
   [{::keys [http-service]
     :as    env}]
+  (assoc env ::http-service (-> http-service
+                                http/create-server
+                                http/start)))
+
+;; app.test
+
+(defonce last-app (atom nil))
+
+(defn ->test-system
+  [{::keys [http-service]
+    :as    env}]
+  (swap! last-app (fn [{::keys [http-service]}]
+                    (when http-service
+                      (http/stop http-service))
+                    (start-system env)))
   (assoc env ::service-fn (-> http-service
-                              (assoc ::http/type :jetty
-                                     ::http/join? false
-                                     ::http/port 8080)
                               http/create-servlet
                               ::http/service-fn)))
-
 (defn gql
   [{::keys [service-fn]} eql]
   (-> eql
       (pcgql/query->graphql {})
       (->> (hash-map :query))
       json/write-str
-      (->> (response-for service-fn :post "/graphql"
+      (->> (response-for service-fn :post "/api"
+                         :headers {"Content-Type" "application/json"}
                          :body))
       :body
       (json/read-str :key-fn keyword)))
+
+;; app.core-test
 
 (deftest api-test
   (let [env (-> (create-system {})
