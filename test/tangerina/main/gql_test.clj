6a39447bf77a78c854f9cc709e5b358b3e1e384b
Ian <d.ian.b@live.com>
2020-05-23 18:43:43 -0300

    adding csk and jsonista

--- test/tangerina/main/gql_test.clj
+++ test/tangerina/main/gql_test.clj
@@ -4,62 +4,65 @@
             [io.pedestal.test :refer [response-for]]
             [datascript.core :as ds]
             [com.walmartlabs.lacinia :as lacinia]
-            [clojure.data.json :as json]
+            [jsonista.core :as json]
+            [camel-snake-kebab.core :as csk]
             [com.walmartlabs.lacinia.schema :as lacinia.schema]
-            [com.wsscode.pathom.connect.graphql2 :as pcgql]
-            [datascript.core :as d]))
+            [com.wsscode.pathom.connect.graphql2 :as pcgql]))
 
+(def mapper
+  (json/object-mapper
+   {:encode-key-fn name
+    :decode-key-fn csk/->kebab-case-keyword}))
 
 (defn create-system
   [{::keys [] :as env}]
-  (let [ds-schema {}
-        conn (ds/create-conn ds-schema)
+  (let [ds-schema      {}
+        conn           (ds/create-conn ds-schema)
         lacinia-schema (-> {:objects   {:Task {:fields {:id          {:type 'Int}
-                                                        :checked     {:type 'Boolean}
-                                                        :description {:type 'String}}}}
-                            :queries   {:tasks {:type    '(list Task)
-                                                :resolve (fn [_ _ _]
-                                                           (->> (d/q '[:find ?e ?description ?checked
-                                                                       :in $
-                                                                       :where
-                                                                       [?e :task/description ?description]
-                                                                       [?e :task/checked? ?checked]]
-                                                                     (d/db conn))
-                                                                (map (partial zipmap [:id :description :checked]))))}}
-                            :mutations {:create_task {:type    'Task
-                                                      :args    {:description {:type 'String}}
-                                                      :resolve (fn [_ {:keys [description]} _]
-                                                                 (let [id (d/tempid :db.part/user)
-                                                                       {:keys [db-after tempids]} (d/transact! conn [{:db/id            id
-                                                                                                                      :task/checked?    false
-                                                                                                                      :task/description description}])]
-                                                                   {:id            (d/resolve-tempid db-after tempids id)
-                                                                    :task/checked? false
-                                                                    :description   description}))}}}
-                           lacinia.schema/compile)
-        http-service (-> {::http/routes #{["/graphql" :post
-                                           [{:name  ::error
-                                             :error (fn [ctx ex]
-                                                      (.printStackTrace ex)
-                                                      (assoc ctx :response {:body   (json/write-str {:ex-message (ex-message ex)})
-                                                                            :status 400}))}
-                                            (fn [{:keys [body] :as req}]
-                                              (let [{:keys [query variables]} (json/read-str (slurp body)
-                                                                                             :key-fn keyword)
-                                                    result (lacinia/execute lacinia-schema query variables req)]
-                                                {:body   (json/write-str result)
-                                                 :status 200}))]
-                                           :route-name ::graphql]}}
-                         http/default-interceptors)]
+                                                       :checked     {:type 'Boolean}
+                                                       :description {:type 'String}}}}
+                           :queries   {:tasks {:type    '(list Task)
+                                               :resolve (fn [_ _ _]
+                                                          (->> (ds/q '[:find ?e ?description ?checked
+                                                                     :in $
+                                                                     :where
+                                                                     [?e :task/description ?description]
+                                                                     [?e :task/checked? ?checked]]
+                                                                   (ds/db conn))
+                                                             (map (partial zipmap [:id :description :checked]))))}}
+                           :mutations {:create_task {:type    'Task
+                                                     :args    {:description {:type 'String}}
+                                                     :resolve (fn [_ {:keys [description]} _]
+                                                                (let [id                     (ds/tempid :db.part/user)
+                                                                      {:keys [db-after tempids]} (ds/transact! conn [{:db/id            id
+                                                                                                                  :task/checked?    false
+                                                                                                                  :task/description description}])]
+                                                                  {:id            (ds/resolve-tempid db-after tempids id)
+                                                                   :task/checked? false
+                                                                   :description   description}))}}}
+                          lacinia.schema/compile)
+        http-service   (-> {::http/routes #{["/graphql" :post
+                                            [{:name  ::error
+                                              :error (fn [ctx ex]
+                                                       (.printStackTrace ex)
+                                                       (assoc ctx :response {:body   (json/write-value-as-string {:ex-message (ex-message ex)} mapper)
+                                                                             :status 400}))}
+                                             (fn [{:keys [body] :as req}]
+                                               (let [{:keys [query variables]} (json/read-value (slurp body) mapper)
+                                                     result                (lacinia/execute lacinia-schema query variables req)]
+                                                 {:body   (json/write-value-as-string result mapper)
+                                                  :status 200}))]
+                                            :route-name ::graphql]}}
+                          http/default-interceptors)]
     (assoc env ::conn conn
-               ::http-service http-service)))
+           ::http-service http-service)))
 
 (defn ->test-system
   [{::keys [http-service]
     :as    env}]
   (assoc env ::service-fn (-> http-service
-                              http/create-servlet
-                              ::http/service-fn)))
+                             http/create-servlet
+                             ::http/service-fn)))
 
 (defn start-system
   "start with (-> (create-system {}) start-system)"
@@ -75,20 +78,20 @@
 (defn gql
   [{::keys [service-fn]} eql]
   (-> eql
-      (pcgql/query->graphql {})
-      (->> (hash-map :query))
-      json/write-str
-      (->> (response-for service-fn :post "/graphql"
-                         :body))
-      :body
-      (json/read-str :key-fn keyword)))
+     (pcgql/query->graphql {})
+     (->> (hash-map :query))
+     (json/write-value-as-string mapper)
+     (->> (response-for service-fn :post "/graphql"
+                      :body))
+     :body
+     (json/read-value mapper)))
 
 (deftest api-test
   (let [env (-> (create-system {})
-                (->test-system))]
+               (->test-system))]
     (is (= (gql env `[{(create_task {:description "world"})
                        [:description]}])
-           {:data {:create_task {:description "world"}}}))
+           {:data {:create-task {:description "world"}}}))
     (is (= (gql env `[{:tasks [:description :checked]}])
            {:data {:tasks [{:checked     false
                             :description "world"}]}}))))
