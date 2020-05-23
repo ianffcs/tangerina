4ec0fb22f916474686c705d80afb3e664165055c
Enzzo Cavallo <souenzzo@gmail.com>
2020-05-23 18:41:52 -0300

    +atom impl

--- test/tangerina/main/gql_test.clj
+++ test/tangerina/main/gql_test.clj
@@ -6,37 +6,63 @@
             [com.walmartlabs.lacinia :as lacinia]
             [clojure.data.json :as json]
             [com.walmartlabs.lacinia.schema :as lacinia.schema]
+            [com.walmartlabs.lacinia.util :as lacinia.util]
             [com.wsscode.pathom.connect.graphql2 :as pcgql]
             [datascript.core :as d]))
+
+(defn atom-impl
+  [{::keys [state]}]
+  (letfn [(next-id []
+            (swap! state update ::last-id (fnil inc 0)))]
+    {:query/tasks          (fn [_ _ _]
+                             (vals (get @state :task/by-id)))
+     :mutation/create-task (fn [_ {:keys [description]} _]
+                             (let [id (next-id)
+                                   task {:id          id
+                                         :checked     false
+                                         :description description}]
+                               (swap! state #(assoc-in % [:task/by-id id] task))
+                               task))}))
+
+
+(defn datascrip-impl
+  [{::keys [conn]}]
+  {:query/tasks          (fn [_ _ _]
+                           (->> (d/q '[:find ?e ?description ?checked
+                                       :in $
+                                       :where
+                                       [?e :task/description ?description]
+                                       [?e :task/checked? ?checked]]
+                                     (d/db conn))
+                                (map (partial zipmap [:id :description :checked]))))
+   :mutation/create-task (fn [_ {:keys [description]} _]
+                           (let [id (d/tempid :db.part/user)
+                                 {:keys [db-after tempids]} (d/transact! conn [{:db/id            id
+                                                                                :task/checked?    false
+                                                                                :task/description description}])]
+                             {:id          (d/resolve-tempid db-after tempids id)
+                              :checked?    false
+                              :description description}))})
+
 
 
 (defn create-system
   [{::keys [] :as env}]
   (let [ds-schema {}
         conn (ds/create-conn ds-schema)
+        state (atom {})
         lacinia-schema (-> {:objects   {:Task {:fields {:id          {:type 'Int}
                                                         :checked     {:type 'Boolean}
                                                         :description {:type 'String}}}}
                             :queries   {:tasks {:type    '(list Task)
-                                                :resolve (fn [_ _ _]
-                                                           (->> (d/q '[:find ?e ?description ?checked
-                                                                       :in $
-                                                                       :where
-                                                                       [?e :task/description ?description]
-                                                                       [?e :task/checked? ?checked]]
-                                                                     (d/db conn))
-                                                                (map (partial zipmap [:id :description :checked]))))}}
+                                                :resolve :query/tasks}}
                             :mutations {:create_task {:type    'Task
                                                       :args    {:description {:type 'String}}
-                                                      :resolve (fn [_ {:keys [description]} _]
-                                                                 (let [id (d/tempid :db.part/user)
-                                                                       {:keys [db-after tempids]} (d/transact! conn [{:db/id            id
-                                                                                                                      :task/checked?    false
-                                                                                                                      :task/description description}])]
-                                                                   {:id            (d/resolve-tempid db-after tempids id)
-                                                                    :task/checked? false
-                                                                    :description   description}))}}}
-                           lacinia.schema/compile)
+                                                      :resolve :mutation/create-task}}}
+                           (lacinia.util/attach-resolvers
+                             (atom-impl {::state state})
+                             #_(datascrip-impl {::conn conn}))
+                           (lacinia.schema/compile))
         http-service (-> {::http/routes #{["/graphql" :post
                                            [{:name  ::error
                                              :error (fn [ctx ex]
@@ -52,6 +78,7 @@
                                            :route-name ::graphql]}}
                          http/default-interceptors)]
     (assoc env ::conn conn
+               ::state state
                ::http-service http-service)))
 
 (defn ->test-system
@@ -89,6 +116,6 @@
     (is (= (gql env `[{(create_task {:description "world"})
                        [:description]}])
            {:data {:create_task {:description "world"}}}))
-    (is (= (gql env `[{:tasks [:description :checked]}])
-           {:data {:tasks [{:checked     false
-                            :description "world"}]}}))))
+    (is (= {:data {:tasks [{:checked     false
+                            :description "world"}]}}
+           (gql env `[{:tasks [:description :checked]}])))))
