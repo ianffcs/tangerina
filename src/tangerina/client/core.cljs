19e292424aabc7a8e75aaadfdb0665d2063686dc
Enzzo Cavallo <souenzzo@gmail.com>
2020-06-25 19:30:10 -0300

    WIP - All operations in one place

--- src/tangerina/client/core.cljs
+++ src/tangerina/client/core.cljs
@@ -7,18 +7,16 @@
 
 (def state (r/atom {:tasks :state/pending}))
 
-(defn check-task [task-cursor]
-  (let [checkbox       (r/atom false)
-        completed?     (:checked @task-cursor)
-        checking       (if completed? "on" "off")
+(defn check-task [{:keys [checked on-completion]}]
+  (let [checkbox (r/atom false)
+        checking (if checked "on" "off")
         check-checkbox #(swap! checkbox not)
-        on-completion  #(gql/check-task! task-cursor)
-        input-props    {:type      "checkbox"
-                        :value     true
-                        :checked   checking
-                        :on-click  check-checkbox
-                        :on-change on-completion}]
-    (if completed?
+        input-props {:type      "checkbox"
+                     :value     true
+                     :checked   checking
+                     :on-click  check-checkbox
+                     :on-change on-completion}]
+    (if checked
       [:span {:class "taskCompletion"}
        [:input input-props]]
       [:span {:class "taskCompletion"}
@@ -40,23 +38,17 @@
             :on-change on-edition}]])
 
 (defn set-description
-  [task-cursor]
-  (let [description   (:description @task-cursor)
-        editing?      (:editing @task-cursor)
-        on-edition    #(swap! task-cursor assoc :description (.. % -target -value))
-        begin-edit    #(swap! task-cursor assoc :editing true)
-        close-edition #(when-not (string/blank? description)
-                         (gql/set-description-task! task-cursor))]
-    (if editing?
-      [:span {:class "taskDescription"
-              :style {:margin "3px"}}
-       [ui-editing-form {::description   description
-                         ::on-edition    on-edition
-                         ::close-edition close-edition}]]
-      [:span {:class "taskDescription"
-              :style {:margin "3px"}}
-       [ui-pre-edit-form {::description description
-                          ::begin-edit  begin-edit}]])))
+  [{:keys [description editing on-edition close-edition begin-edit]}]
+  (if editing
+    [:span {:class "taskDescription"
+            :style {:margin "3px"}}
+     [ui-editing-form {::description   description
+                       ::on-edition    on-edition
+                       ::close-edition close-edition}]]
+    [:span {:class "taskDescription"
+            :style {:margin "3px"}}
+     [ui-pre-edit-form {::description description
+                        ::begin-edit  begin-edit}]]))
 
 (defn delete-task
   [{:keys [delete on-deletion]}]
@@ -81,18 +73,43 @@
 
 (defn task-template
   [task-cursor]
-  (let [{:keys [delete id] :as task} @task-cursor
+  (let [{:keys [delete id description] :as task} @task-cursor
         on-deletion (fn []
                       ;; otimist update
                       (swap! task-cursor assoc :delete true)
                       (gql/execute! `[{(deleteTask {:id ~id})
-                                       [:id :checked :description]}]))]
+                                       [:id :checked :description]}]))
+        on-edition #(swap! task-cursor assoc :description %)
+        begin-edit #(swap! task-cursor assoc :editing true)
+        close-edition #(when-not (string/blank? description)
+                         (swap! task-cursor dissoc :editing)
+                         (let [req (gql/execute! `[{(setDescriptionTask {:id ~id :description ~description})
+                                                    [:description :checked :id]}])]
+                           (async/go
+                             (->> req
+                                  async/<!
+                                  :setDescriptionTask
+                                  (reset! task)))))
+        on-completion (fn []
+                        ;; optimist
+                        (swap! task-cursor update :checked not)
+                        (let [req (gql/execute! `[{(checkTask {:id ~id})
+                                                   [:id :checked :description]}])]
+                          (async/go
+                            (->> req
+                                 async/<!
+                                 :completeTask
+                                 (reset! task-cursor)))))]
     (if delete
       [:<>]
       [:<>
-       [check-task task-cursor]
+       [check-task (assoc task
+                     :on-completion on-completion)]
        [ui-id task] " "
-       [set-description task-cursor]
+       [set-description (assoc task
+                          :on-edition on-edition
+                          :begin-edit begin-edit
+                          :close-edition close-edition)]
        [delete-task (assoc task
                       :on-deletion on-deletion)]])))
 
